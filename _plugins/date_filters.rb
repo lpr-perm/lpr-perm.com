be02c5b6a6f1c1ac7c08cf736e026bb9be7bab7c
Alex Kotov <kotovalexarian@gmail.com>
2018-11-22 21:07:09 +0500

    Check datetime format

--- _plugins/date_filters.rb
+++ _plugins/date_filters.rb
@@ -1,7 +1,13 @@
 module Jekyll
   module Filters
     module DateFilters
+      DATE_RE = /\A\d{4}-\d\d-\d\d \d\d:\d\d:\d\d [+-]\d{4}\z/
+
       def date_to_xmlschema(date)
+        date = date.to_s if date.is_a? Time
+        raise TypeError unless date.is_a? String
+        raise 'Invalid format' unless date =~ DATE_RE
+
         custom_time(date).xmlschema
       end
 
