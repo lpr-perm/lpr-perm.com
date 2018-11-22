8322d548f37a8705d12c3a6381632fda4152d791
Alex Kotov <kotovalexarian@gmail.com>
2018-11-22 21:00:08 +0500

    Override module Jekyll::Filters::DateFilters

---
+++ _plugins/date_filters.rb
@@ -0,0 +1,40 @@
+module Jekyll
+  module Filters
+    module DateFilters
+      def date_to_xmlschema(date)
+        custom_time(date).xmlschema
+      end
+
+    private
+
+      def custom_time(input)
+        date = custom_to_date input
+        unless date.respond_to? :to_time
+          raise Errors::InvalidDateError,
+                "Invalid Date: '#{input.inspect}' is not a valid datetime."
+        end
+        date.to_time.dup.localtime
+      end
+
+      def custom_to_date(obj)
+        return obj if obj.respond_to?(:strftime)
+
+        if obj.is_a?(String)
+          return nil if obj.empty?
+          obj = obj.downcase
+        end
+
+        case obj
+        when 'now'.freeze, 'today'.freeze
+          Time.now
+        when /\A\d+\z/, Integer
+          Time.at(obj.to_i)
+        when String
+          Time.parse(obj)
+        end
+      rescue ::ArgumentError
+        nil
+      end
+    end
+  end
+end
