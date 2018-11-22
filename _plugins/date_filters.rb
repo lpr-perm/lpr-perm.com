ce701701b5a0c8daba05f463bcb1b9349ab810ea
Alex Kotov <kotovalexarian@gmail.com>
2018-11-22 21:09:46 +0500

    Rename arg

--- _plugins/date_filters.rb
+++ _plugins/date_filters.rb
@@ -3,12 +3,12 @@ module Jekyll
     module DateFilters
       DATE_RE = /\A\d{4}-\d\d-\d\d \d\d:\d\d:\d\d [+-]\d{4}\z/
 
-      def date_to_xmlschema(date)
-        date = date.to_s if date.is_a? Time
-        raise TypeError unless date.is_a? String
-        raise 'Invalid format' unless date =~ DATE_RE
+      def date_to_xmlschema(value)
+        value = value.to_s if value.is_a? Time
+        raise TypeError unless value.is_a? String
+        raise 'Invalid format' unless value =~ DATE_RE
 
-        Time.parse(date).to_time.localtime.xmlschema
+        Time.parse(value).to_time.localtime.xmlschema
       end
     end
   end
