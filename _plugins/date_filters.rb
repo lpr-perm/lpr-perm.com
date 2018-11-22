bb2ed5aee314284ab395270c560dcc32357b8d53
Alex Kotov <kotovalexarian@gmail.com>
2018-11-22 21:09:11 +0500

    Improve code

--- _plugins/date_filters.rb
+++ _plugins/date_filters.rb
@@ -8,38 +8,7 @@ module Jekyll
         raise TypeError unless date.is_a? String
         raise 'Invalid format' unless date =~ DATE_RE
 
-        custom_time(date).xmlschema
-      end
-
-    private
-
-      def custom_time(input)
-        date = custom_to_date input
-        unless date.respond_to? :to_time
-          raise Errors::InvalidDateError,
-                "Invalid Date: '#{input.inspect}' is not a valid datetime."
-        end
-        date.to_time.dup.localtime
-      end
-
-      def custom_to_date(obj)
-        return obj if obj.respond_to?(:strftime)
-
-        if obj.is_a?(String)
-          return nil if obj.empty?
-          obj = obj.downcase
-        end
-
-        case obj
-        when 'now'.freeze, 'today'.freeze
-          Time.now
-        when /\A\d+\z/, Integer
-          Time.at(obj.to_i)
-        when String
-          Time.parse(obj)
-        end
-      rescue ::ArgumentError
-        nil
+        Time.parse(date).to_time.localtime.xmlschema
       end
     end
   end
